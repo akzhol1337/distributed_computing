6bbc46ab2843662c56ef0cf80a44bb925dfab65e
Akzhol Issaliyev <akzhol.issaliyev@folium.cloud>
2022-10-26 04:28:20 +0600

    Multithreaded

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -2,11 +2,16 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Simple web server.
  */
 public class WebServer {
+
+    private static final int PARALLELISM_LEVEL = 7;
+
     public static void main(String[] args) {
         // Port number for http request
         int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
@@ -17,26 +22,10 @@ public class WebServer {
             System.out.println("Web Server is starting up, listening at port " + port + ".");
             System.out.println("You can access http://localhost:" + port + " now.");
 
+            ExecutorService executor = Executors.newFixedThreadPool(PARALLELISM_LEVEL);
+
             while (true) {
-                // Make the server socket wait for the next client request
-                Socket socket = serverSocket.accept();
-
-                System.out.println("Got connection!");
-
-                // To read input from the client
-                BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-
-                try {
-                    // Get request
-                    HttpRequest request = HttpRequest.parse(input);
-
-                    // Process request
-                    Processor proc = new Processor(socket, request);
-                    proc.process();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                executor.submit(new RequestHandler(serverSocket.accept()));
             }
         }
         catch (IOException ex) {
